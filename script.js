eb7bf5383cbc1dd648a9e3c2a4f2c4904f5ac75e
Arthur Maciel <134110407+S0L4RT@users.noreply.github.com>
2023-12-14 21:32:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const enviar = document.querySelector(".botao");
+enviar.addEventListener("click", function(){
+    alert("Sua solicitação de exame foi enviada com sucesso! Aguarde as informações da sua consulta no seu e-mail.")})
